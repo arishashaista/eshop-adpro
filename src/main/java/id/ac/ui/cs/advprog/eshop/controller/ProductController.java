a04f02131b79b03402dea211063e263fe2cf5892
arishashaista <arishashaistaa@gmail.com>
2025-02-17 19:28:37 +0700

    fix the return of ProductController.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -24,7 +24,7 @@ public class ProductController {
     public String createProductPage(Model model) {
         Product product = new Product();
         model.addAttribute("product", product);
-        return "createProduct";
+        return "CreateProduct";
     }
 
     @PostMapping("/create")
@@ -37,7 +37,7 @@ public class ProductController {
     public String productListPage(Model model) {
         List<Product> allProducts = service.findAll();
         model.addAttribute("products", allProducts);
-        return "productList";
+        return "ProductList";
     }
 
     @GetMapping("/edit/{productId}")
@@ -46,7 +46,7 @@ public class ProductController {
         Product product = service.findById(productId);
         if (product != null) {
             model.addAttribute("product", product);
-            return "editProduct";
+            return "EditProduct";
         }
         return "redirect:/product/list";
     }
